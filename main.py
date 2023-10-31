6675c2b797dcb315872bd080361fdd95944f1ef4
Ramyro Corrêa Aquines <ramyrocorrea@gmail.com>
2023-10-30 21:06:16 -0300

    Adicionando comentários ao código.

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-class Produto:
-  def __init__(self, codigo_barras, nome, preco):
-      self.codigo_barras = codigo_barras
+class Produto: #Crio a classe Produto, com cada produto tendo um código de barras, nome e preço.
+  def __init__(self, codigo_barras, nome, preco): 
+      self.codigo_barras = codigo_barras #Defino os atributos desejados.
       self.nome = nome
       self.preco = preco
 
-class ProdutoFisico(Produto):
-  def __init__(self, codigo_barras, nome, preco, peso):
+#Agora, para cada produto, o classifico em Produto Físico, Produto Digital ou Produto Virtual.
+class ProdutoFisico(Produto): 
+  def __init__(self, codigo_barras, nome, preco, peso): #Para um Produto Físico, adiciono o atributo peso. 
       super().__init__(codigo_barras, nome, preco)
       self.peso = peso
 
 class ProdutoDigital(Produto):
-  def __init__(self, codigo_barras, nome, preco, tamanho_arquivo):
+  def __init__(self, codigo_barras, nome, preco, tamanho_arquivo): #Para um Produto Digital, adiciono o atributo tamanho_arquivo.
       super().__init__(codigo_barras, nome, preco)
       self.tamanho_arquivo = tamanho_arquivo
 
 class ProdutoVirtual(Produto):
-  def __init__(self, codigo_barras, nome, preco, link_download):
+  def __init__(self, codigo_barras, nome, preco, link_download): #Pra um Produto Virtual, adiciono o atributo link_download.
       super().__init__(codigo_barras, nome, preco)
       self.link_download = link_download
 
@@ -25,17 +26,17 @@ class ProdutoNaoEncontradoException(Exception):
 class QuantidadeInsuficienteException(Exception):
   pass
 
-class Inventario:
-  def __init__(self):
+class Inventario: #Crio a classe do Inventário.
+  def __init__(self): #Gero a lista de produtos do Inventário.
       self.produtos = []
 
-  def adicionar_produto(self, produto, quantidade):
+  def adicionar_produto(self, produto, quantidade): #Gero a ação de adicionar um novo produto (com a respectiva quantidade) ao Inventário.
       self.produtos.extend([produto] * quantidade)
 
-  def vender_produto(self, codigo_barras):
+  def vender_produto(self, codigo_barras): #Gero a ação de vender um produto do Inventário.
       for produto in self.produtos:
           if produto.codigo_barras == codigo_barras:
-              self.produtos.remove(produto)
+              self.produtos.remove(produto) #Retiro o produto da lista de produtos do Inventário.
               return produto
       raise ProdutoNaoEncontradoException("Produto não encontrado no estoque.")
 
@@ -43,8 +44,9 @@ class Inventario:
       self.produtos.append(produto)
     
   def obter_informacoes_inventario(self):
-    produtos_diferentes = {}  # Usaremos um dicionário para armazenar produtos distintos e suas quantidades
+    produtos_diferentes = {}  # Usaremos um dicionário para armazenar produtos distintos e suas quantidades.
 
+    #Verificamos a instância do produto.
     for produto in self.produtos:
       if isinstance(produto, ProdutoFisico):
           tipo_produto = "Produto Físico"
@@ -57,8 +59,8 @@ class Inventario:
 
       chave_produto = (produto.codigo_barras, tipo_produto)  # Usamos uma tupla como chave do dicionário
       if chave_produto in produtos_diferentes:
-          produtos_diferentes[chave_produto]["quantidade"] += 1
-      else:
+          produtos_diferentes[chave_produto]["quantidade"] += 1 #Se o produto já está na lista, aumento a quantidade em 1.
+      else: #Caso não esteja, atribuímos os atributos desejados ao produto e o adicionamos na lista.
           produtos_diferentes[chave_produto] = {
               "nome": produto.nome,
               "preco": produto.preco,
@@ -66,7 +68,7 @@ class Inventario:
               "informacoes_adicionais": produto.peso if isinstance(produto, ProdutoFisico) else produto.tamanho_arquivo if isinstance(produto, ProdutoDigital) else produto.link_download
           }
 
-    informacoes = []
+    informacoes = [] #Crio uma lista com informações do inventário.
     for chave, produto_info in produtos_diferentes.items():
       informacoes.append(f"{chave[1]} - Código de Barras: {chave[0]}, Nome: {produto_info['nome']}, Preço: {produto_info['preco']}, Quantidade: {produto_info['quantidade']}, Informações Adicionais: {produto_info['informacoes_adicionais']}")
     return informacoes
@@ -82,7 +84,7 @@ if __name__ == "__main__":
   inventario.adicionar_produto(produto2, 5)
   inventario.adicionar_produto(produto3, 3)
 
-  while True:
+  while True: #Criando um menu interativo.
       print("\nOpções:")
       print("1. Vender Produto")
       print("2. Adicionar Produto")
@@ -90,12 +92,13 @@ if __name__ == "__main__":
       print("4. Consultar Inventário")
       escolha = input("Escolha uma opção: ")
 
+        #Para cada escolha no menu, criamos a interatividade desejada.
       if escolha == "1":
           codigo_barras = input("Digite o código de barras do produto a ser vendido: ")
           try:
               produto_vendido = inventario.vender_produto(codigo_barras)
               print(f"Produto vendido: {produto_vendido.nome}")
-          except ProdutoNaoEncontradoException:
+          except ProdutoNaoEncontradoException: #Tratamento de exceção.
               print("Produto não encontrado no estoque.")
           except QuantidadeInsuficienteException:
               print("Quantidade insuficiente em estoque.")
